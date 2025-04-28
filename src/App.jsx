fc1f3bd1d49a02e2d0c30e1ad7c84acc5ade9afd
Vaibhav Shete <vaibhav@intellifysolutions.com>
2025-04-28 09:32:53 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,9 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [post, setPost] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-        setPost(res.data);
-        console.log(res.data, "%%%%");
-      } catch (err) {
-        console.error("Error fetching posts:", err);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div>
-      <h1>Fetched Posts</h1>
-      {post.slice(0, 5).map((item) => (
-        <div key={item.id}>
-          <h3>{item.title}</h3>
-          <p>{item.body}</p>
-        </div>
-      ))}
+      <h1 className="text-3xl font-bold underline">Portfolio webiste</h1>
     </div>
   );
 }
